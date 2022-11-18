12d62bebc33190736b78e12d7b48b6fc229e7194
brycelino <brycejeff16@gmail.com>
2022-11-17 19:41:14 -0600

    TTTRebuild
    a) If the computer can win immediately, it should do so
    
    b) If the human's next move would result in a victory, the AI should choose that square to block
    
    c) If the center square is open, choose it
    
    d) If any of the corner squares are open, choose one of them
    
    e) Choose any open square at random

---
+++ TTT 4.27/Intermediate/Build/Win64/TTTEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.26.28806
+#include "C:/Users/broli/Desktop/TicTacToe-main/TicTacToe-main/TTT 4.27/Intermediate/Build/Win64/TTTEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
